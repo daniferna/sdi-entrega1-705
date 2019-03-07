60771716d76cc01b0496ad6042a4c53f4ac9593f
Daniel <daniferna@outlook.com>
2019-03-07 10:46:39 +0100

    Comienzo de nuevo, basandome en el proyecto de clase

---
+++ src/main/java/com/uniovi/Wallapop.java
@@ -0,0 +1,14 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wallapop {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wallapop.class, args);
+    }
+
+}
+
